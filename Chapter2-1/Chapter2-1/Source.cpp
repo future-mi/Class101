03ada365a3695aa9b892fe4013ab6b15b06c3fce
future <83049262+future-mi@users.noreply.github.com>
2023-12-26 22:06:54 +0900

    2023-04-09

---
+++ Chapter2-1/Chapter2-1/Source.cpp
@@ -0,0 +1,61 @@
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+/*
+int main() {
+	int number = 100 + 50;
+	int result; 
+	result = number++;
+
+	printf("result : %d", result);
+	
+	number++;
+	++number;
+
+	return 0;
+}
+*/
+
+//int main() {
+//	int person = 100;
+//	int animal = 100;
+//
+//	int result = person == animal;
+//	printf("result : %d", result);
+//	/*
+//	if (person != animal) {
+//		printf("사람과 동물은 다릅니다");
+//	}
+//	*/
+//	return 0;
+//}
+
+//int main() {
+//	int number = 100 + 50;
+//	int result;
+//
+//	result = ++number;
+//
+//	++number;
+//
+//	printf("result: %d", result);
+//
+//	return 0;
+//}
+
+int main() {
+	//if (97 == 'a') {
+
+	//	printf("97은 'a'입니다");
+
+	//	return 0;
+	//}
+
+	float pi = 3.14;
+	printf("%d", pi);
+
+	return 0;
+
+}
